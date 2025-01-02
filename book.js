ea0dda8b98bfe0342e74ef2552e606aaa3f5a4fa
NazeerMalik <nazeermalik23456@gmail.com>
2025-01-02 11:33:02 +0500

    first commit

---
+++ book.js
@@ -0,0 +1,39 @@
+let menu = document.querySelector('#navmenu');
+let nav = document.querySelector('#menu');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  nav.classList.toggle('open');
+};
+
+function form() {
+  var name = document.getElementById('name').value.trim();
+  var number = document.getElementById('number').value.trim();
+  var email = document.getElementById('email').value.trim();
+  var password = document.getElementById('password').value.trim();
+  var message = document.getElementById('message').value.trim();
+
+  if (name.length === 0) {
+    alert('Please enter your name.');
+    return false;
+  }
+  if (number.length === 0) {
+    alert('Please enter your number.');
+    return false;
+  }
+  if (email.length === 0) {
+    alert('Please enter your email.');
+    return false;
+  }
+  if (password.length === 0) {
+    alert('Please enter your password.');
+    return false;
+  }
+  if (message.length === 0) {
+    alert('Please enter your message.');
+    return false;
+  }
+  document.getElementById('form').reset();
+  alert('Contact form submitted successfully.');
+  return true;
+}
